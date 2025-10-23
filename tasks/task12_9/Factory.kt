d0c2ab7b12117dfce5ba22dc8f324912e315bc4b
chestercb29@gmail.com <105993308+Chester-Booth@users.noreply.github.com>
2025-10-23 16:37:00 +0100

    12.9 c# static, object delacrations

--- tasks/task12_9/Factory.kt
+++
@@ -1,5 +0,0 @@
-// Task 12.9: class implementing the Static Factory Method pattern
-
-import java.time.LocalDate
-
-class Person(val name: String, val birth: LocalDate)
---
+++ tasks/task12_9/src/Factory.kt
@@ -0,0 +1,21 @@
+// Task 12.9: class implementing the Static Factory Method pattern
+
+import java.time.LocalDate
+
+class Person private constructor(val name: String, val birth: LocalDate) {
+    companion object Factory {
+        private val names = mutableSetOf<String>()
+
+        fun create(name: String, birth: LocalDate): Person {
+            require(name !in names) { "Name must be unique!" }
+            names.add(name)
+            return Person(name, birth)
+        }
+    }
+}
+
+
+fun main(){
+    val p = Person.create("Guy", LocalDate.of(2005, 7, 16))
+    val p2 = Person.create("Guy", LocalDate.of(2006,6,29))
+}
