9483abe0ae45cba60b3dc47b8f1e3da0f2801c9a
chestercb29@gmail.com <105993308+Chester-Booth@users.noreply.github.com>
2025-10-30 15:22:31 +0000

    17.3 interfaces example

--- tasks/task17_3/src/Interfaces.kt
+++ tasks/task17_3/src/Interfaces.kt
@@ -1,8 +1,17 @@
 // Task 17.3: creating & using interfaces
 
 // Add Printable interface here
+interface Printable {
+    fun print()
+}
+
 
 // Add Document class here
+class Document (val filename: String): Printable{
+    override fun print() {
+        println("printing: $filename")
+    }
+}
 
 fun main() {
     val item: Printable = Document("info.txt")
